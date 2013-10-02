104e2f8d858b271208ef3a25643b4b8f12775edd
Jonathan B <bejonster@gmail.com>
2013-10-03 00:12:00 +0200

    creates jQuery plugin

---
+++ js/progressbar.js
@@ -0,0 +1,24 @@
+!function ($) {
+
+    "use strict";
+
+    // PROGRESSBAR CLASS DEFINITION
+    // ============================
+
+    var Progressbar = function (element) {
+        this.$element = $(element);
+    }
+
+    // PROGRESSBAR PLUGIN DEFINITION
+    // =============================
+
+    $.fn.progressbar = function () {
+        return this.each(function () {
+            var $this = $(this),
+                data = $this.data('jbl.progressbar');
+
+            if (!data) $this.data('jbl.progressbar', (data = new Progressbar(this)));
+        })
+    };
+
+}(window.jQuery);
